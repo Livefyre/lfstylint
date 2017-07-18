8da97210acd8a7a56f5d48f2e951bfd325199a3d
Devon Govett <devongovett@gmail.com>
2017-07-17 19:43:29 -0700

    Fixing

--- index.js
+++ index.js
@@ -2,8 +2,4 @@
 
 var stylint = require('stylint');
 var config = require('./config.json');
-if (process.argv.length > 2) {
-  stylint().create({}, config, process.argv.slice(2));
-} else {
-  stylint(process.cwd(), config).create();
-}
+stylint(process.argv.length > 2 ? process.argv.slice(2) : process.cwd(), config).create();
